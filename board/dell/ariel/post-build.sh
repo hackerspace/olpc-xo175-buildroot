b41e1987dc76711e3a24fec35b08f58dc21d6122
Lubomir Rintel <lkundrak@v3.sk>
2019-10-31 11:53:28 +0100

    configs,board/dell/ariel: add Dell Ariel configuration
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>

---
+++ board/dell/ariel/post-build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+BOARD_DIR="$(dirname $0)"
+
+for i in tty1 tty2 tty3 tty4
+do
+	mkdir -p $TARGET_DIR/etc/systemd/system/getty.target.wants
+	ln -sf ../../../../usr/lib/systemd/system/getty@.service \
+		$TARGET_DIR/etc/systemd/system/getty.target.wants/getty@$i.service
+done
+
+ln -sf /var/run/NetworkManager/resolv.conf $TARGET_DIR/etc/resolv.conf
+
+rm -f $TARGET_DIR/etc/systemd/system/multi-user.target.wants/systemd-networkd.service
+rm -f $TARGET_DIR/etc/systemd/system/network-online.target.wants/systemd-networkd-wait-online.service
+rm -f $TARGET_DIR/etc/systemd/system/sockets.target.wants/systemd-networkd.socket
+
+install -pm 0644 -D $BOARD_DIR/wloader.cfg $BINARIES_DIR/wloader.cfg
