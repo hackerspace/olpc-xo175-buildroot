04ef399cfc9875c70075e0dda850555a80da3c36
Lubomir Rintel <lkundrak@v3.sk>
2020-03-23 09:37:19 +0100

    Revert "mesh"
    This reverts commit 53d8ee01787d4c4db3e3baa59073555b26174aab.

--- board/olpc/post-build.sh
+++ board/olpc/post-build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 BOARD_DIR="$(dirname $0)"
 
 for i in tty1 tty2 tty3 tty4
@@ -17,35 +15,5 @@ rm -f $TARGET_DIR/etc/systemd/system/multi-user.target.wants/systemd-networkd.se
 rm -f $TARGET_DIR/etc/systemd/system/network-online.target.wants/systemd-networkd-wait-online.service
 rm -f $TARGET_DIR/etc/systemd/system/sockets.target.wants/systemd-networkd.socket
 rm -rf $TARGET_DIR/var/log/journal
-
-mkdir -p $TARGET_DIR/etc/modprobe.d
-cat >$TARGET_DIR/etc/modprobe.d/libertas.conf <<EOF
-# Prefer the "Thin Firmware" soft MAC driver
-blacklist usb8xxx
-#blacklist libertas_tf_usb
-EOF
-
-(
-umask 0077
-cat >>$TARGET_DIR/usr/lib/NetworkManager/system-connections/mesh1.nmconnection <<EOF
-[connection]
-id=mesh1
-uuid=7dea1207-aed0-4f0f-8f08-22927e82f64f
-type=wifi
-
-[wifi]
-band=bg
-channel=1
-mode=mesh
-ssid=Mesh
-
-[ipv4]
-method=link-local
-
-[ipv6]
-addr-gen-mode=stable-privacy
-method=link-local
-EOF
-)
 
 install -m 0644 -D $BOARD_DIR/olpc.fth $TARGET_DIR/boot/olpc.fth
--- board/olpc/post-image.sh
+++ board/olpc/post-image.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 BOARD_DIR="$(dirname $0)"
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
