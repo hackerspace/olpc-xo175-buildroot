bbb64f704f71cf89be132a28db1749c533c64883
Lubomir Rintel <lkundrak@v3.sk>
2020-03-23 19:01:44 +0000

    XXX changes for mmp3 testing build, kernels + roms

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -134,7 +134,7 @@ define GLIBC_CONFIGURE_CMDS
 		--disable-profile \
 		--without-gd \
 		--enable-obsolete-rpc \
-		--enable-kernel=$(call qstrip,$(BR2_TOOLCHAIN_HEADERS_AT_LEAST)) \
+		--enable-kernel=3.5 \
 		--with-headers=$(STAGING_DIR)/usr/include)
 	$(GLIBC_ADD_MISSING_STUB_H)
 endef
